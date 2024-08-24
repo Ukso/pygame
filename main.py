7c0446d1fa76c0e21955a58d927568f702afb591
Ukso <oskari.vierijarvi@gmail.com>
2024-08-24 12:10:03 +0300

    init

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init
     print ("Starting asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        
+    game_loop()
+
+def game_loop():
+    Clock = pygame.time.Clock()
+    dt = 0
+    while True:
+        pygame.display.flip()
+
+
+
+        dt = Clock.tick(30)/1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__=="__main__":
     main()
